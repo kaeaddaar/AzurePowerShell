f9139be6144bbdba3f60800f056b8dadb9fbb473
kaeaddaar <playinfc@hotmail.ca>
2017-02-19 02:48:21 -0800

    Solid core functions in place

---
+++ AzureVM/UsingPortalFunctions.ps1
@@ -0,0 +1,20 @@
+﻿$Array1 = "One", "Two", "Three", "Four", "Five", "Six", "Seven"
+$Array1 | Enum-String | Format-Columnnize -ColumnNum 3
+
+$lstImagePublisher = Get-AzureRmVMImagePublisher -Location "westus2" | Select-Object -expandProperty PublisherName
+$lstImagePublisher | Enum-String | Format-Columnnize 3
+$intImagePublisher = Read-Host "Pick image publisher from list"
+$lstImagePublisher[$intImagePublisher]
+cls
+
+# Get the location list
+$lstLocation = Get-AzureRmlocation | Select-Object -ExpandProperty Location
+# Display the location list
+$lstLocation | Enum-String | Format-Columnnize 3
+# Wrap picking in a simple function
+Function get-PLocation { $lstLocation.Item((Read-Host "Pick location from list"))}
+# Place function below where you want to get the location
+Write-Host "the location is" (Get-PLocation)
+
+
+
