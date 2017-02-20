2f5d87e2fd2838f8c347afed52556e609352dfb1
kaeaddaar <playinfc@hotmail.ca>
2017-02-20 01:21:33 -0800

    I finished up proof of concept of portal functionality
    The Make-Key Portal.txt contains the code to create the text block so we
    can create a new-IseSnippet. PortalFunctionality.psm1 should be placed
    in the appropriate folder. UsingPortalFunctions.ps1 contains most of the
    code used as I designed how to implement the desired functionality
    elegantly.

---
+++ AzureVM/PortalFunctionality.psm1
@@ -0,0 +1,229 @@
+﻿<#
+.Synopsis
+   Enumerate strings
+.DESCRIPTION
+   Enumerate strings
+.EXAMPLE
+   $lstImagePublisher | Enum-String
+.EXAMPLE
+   The following example shows how to get locations into a list of locations and output it in columns
+($lstLocation = Get-AzureRmlocation | Select-Object -ExpandProperty Location) | Enum-String| Format-Columnnize 3
+#>
+function global:Enum-String
+{
+    [CmdletBinding()]
+    [Alias("E-S")]
+    [OutputType([string])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [String]
+        $Name
+    )
+
+    Begin
+    {
+        $Arr = New-Object System.Collections.ArrayList
+        $i = 0
+    }
+    Process
+    {
+        $T = $Arr.Add(" " + $i + " " + $Name)
+        $i++        
+    }
+    End
+    {
+        $Arr
+    }
+}
+
+
+<#
+.Synopsis
+   Get the nth Item from piped objects
+.DESCRIPTION
+   Get the nth Item from piped objects
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function global:get-nthItem
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([string])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$true,
+                   Position=1)]
+        [Object]
+        $Obj,
+
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        [Int]
+        $Position
+    )
+
+    Begin
+    {
+        $i = 0
+    }
+    Process
+    {
+        if ($i -eq $Position)
+        {
+            $obj
+        }
+        $i++        
+    }
+    End
+    {
+        
+    }
+}
+
+
+<#
+.Synopsis
+   Enumerate strings
+.DESCRIPTION
+   Enumerate strings
+.EXAMPLE
+   Get-AzureRmVMImagePublisher -Location "westus2" | Select-Object -expandProperty PublisherName | Enum-String | Format-Columnnize -ColumnNum 3
+.EXAMPLE
+    $Array1 = "One", "Two", "Three", "Four", "Five", "Six", "Seven"
+    $Array1 | Enum-String | Format-Columnnize -ColumnNum 3
+Results:
+    Col0     Col1    Col2    
+    ----     ----    ----    
+     0 One    1 Two   2 Three
+     3 Four   4 Five  5 Six  
+     6 Seven                 
+#>
+function global:Format-Columnnize
+{
+    [CmdletBinding()]
+    [Alias("F-C")]
+    [OutputType([string])]
+    Param
+    (
+        # Name is the default column to match by for piping
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   ValueFromPipeline=$true,
+                   Position=1)]
+        [String]
+        $Name,
+
+        # ColumnNum is the number of columns you want to use in the table
+        [Parameter(Mandatory=$true,
+                   Position=0)]
+        $ColumnNum
+
+    )
+
+    Begin
+    {
+        $Arr = New-Object System.Collections.ArrayList
+        $Row = New-Object psobject
+        $i = 0
+        $i2 = 0
+    }
+    Process
+    {
+        if ($i2 -ge $ColumnNum) 
+        {
+            $i2 = 0
+            $T = $Arr.Add($Row)
+            $Row = New-Object psobject
+        }
+        Add-Member -InputObject $Row -MemberType NoteProperty -Name "Col$i2" -Value $Name
+        $i2++
+        $i++        
+    }
+    End
+    {
+        While ($i2 -lt $ColumnNum)
+        {
+            Add-Member -InputObject $Row -MemberType NoteProperty -Name "Col$i2" -Value ""
+            $i2++
+        }
+        if ($i2 = $ColumnNum) 
+        { 
+            $T = $Arr.Add($Row)
+        }
+        $Arr
+    }
+}
+
+
+<#
+.Synopsis
+   Adds a function to a hash table
+.DESCRIPTION
+   Makes a key in a hash table. Supports pipeline and makes sure the key exists before we start setting them
+.EXAMPLE
+   $HashVar = @{"Key1"="Val1"}
+   Add-Member-MakeKeyScript -Hash $HashVar
+   $HashVar.'Key2' = "Val2"
+   $HashVar
+Results: 
+Name                           Value                                                                                           
+----                           -----                                                                                           
+Key1                           Val1                                                                                            
+Key2                           Val2                                                                                            
+
+.EXAMPLE
+   $HashVar = @{"Key1"="Val1"}
+   $HashVar2 = @{"Key1"="Val1"}
+   ($HashVar2, $HashVar) | Add-Member-MakeKeyScript
+   $HashVar2.'Key2' = "Val2"
+   $HashVar.'Key2' = "Val2"
+   $HashVar2
+   $HashVar
+Results: 
+Name                           Value                                                                                           
+----                           -----                                                                                           
+Key1                           Val1                                                                                            
+Key2                           Val2                                                                                            
+Key1                           Val1                                                                                            
+Key2                           Val2                                                                                            
+
+#>
+Function global:Add-Member-MakeKeyScript
+{
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory=$True, 
+                     ValueFromPipelineByPropertyName=$true,
+                     ValueFromPipeline=$true, 
+                     Position=0)]
+        [Hashtable]$Hash
+    )
+    Begin
+    {
+        $ScriptToMakeKey = {[CmdletBinding()] param([Parameter(Mandatory=$True, ValueFromPipelineByPropertyName=$true, ValueFromPipeline=$true, Position=0)] [String]$Name)if (-not $this.ContainsKey($Name)) {$this.Add("$Name","") }}
+    }
+
+    Process
+    {
+        if (-not(Get-Member -inputobject $Hash -name "Make-Key" -Membertype ScriptMethod))
+        {
+            Add-Member -InputObject $Hash -Name "Make-Key" -MemberType ScriptMethod -Value $ScriptToMakeKey
+        }
+    }
+
+    End {$ScriptToMakeKey = $null}
+}
+
+
--- AzureVM/UsingPortalFunctions.ps1
+++ AzureVM/UsingPortalFunctions.ps1
@@ -12,9 +12,49 @@ $lstLocation = Get-AzureRmlocation | Select-Object -ExpandProperty Location
 # Display the location list
 $lstLocation | Enum-String | Format-Columnnize 3
 # Wrap picking in a simple function
-Function get-PLocation { $lstLocation.Item((Read-Host "Pick location from list"))}
+Function get-PLocation 
+{ 
+    $lstLocation = Get-AzureRmlocation | Select-Object -ExpandProperty Location
+    $lstLocation | Enum-String| Format-Columnnize 3
+    $lstLocation.Item((Read-Host -Prompt "Enter Number of Location")).Location
+}
 # Place function below where you want to get the location
 Write-Host "the location is" (Get-PLocation)
 
 
+
+
+($lstLocation = Get-AzureRmlocation | Select-Object -ExpandProperty Location) | Enum-String| Format-Columnnize 3
+$lstLocation = $null
+
+Add-Member -InputObject $PSO -Name "test2" -MemberType ScriptMethod -Value {param ($Param1);Write-Host $this.Location + $Param1 + "stuff"} -PassThru
+
+# Lets create a way to track settings, using the previously set settings by default
+$HSettings = @{"VmName"="VmTest1"}
+
+# I want to pass a non-blank setting, and if it doesn't exist to add it, else change the setting
+# if setting is blank, we can treat things like the portal and enumerate the options
+
+$ScriptToMakeKey = {[CmdletBinding()] param([Parameter(Mandatory=$True, ValueFromPipelineByPropertyName=$true, ValueFromPipeline=$true, Position=0)] [String]$Name)if (-not $this.ContainsKey($Name)) {$this.Add("$Name","") }}
+Add-Member -InputObject $HSettings -Name "Make-Key" -MemberType ScriptMethod -Value $ScriptToMakeKey
+$HSettings.'Make-Key'("Var1")
+$HSettings.var1 = "Hello WOrld"
+
+get-help Add-Member -full
+$HSettings | gm
+
+# ----- Time to write some sample code -----
+$HSettings = @{"VmName"="TestVm1"}
+$HSettings | Add-Member-MakeKeyScript
+
+# Location is needed for Get-AzureRmVmImagePublisher
+$HSettings.'Make-Key'("Location")
+#if location is blank, grab locations from azure
+if ($HSettings.Location.Length -eq 0)
+{
+    ($lstLocation = Get-AzureRmLocation | Select-Object -ExpandProperty Location) | Enum-String | Format-Columnnize 3
+    Read-Host "Press any key to see list of locations"
+    $HSettings.Location = $lstLocation.Item((READ-host "Select number of a location"))
+}
+$HSettings.Location
 
