5a3b5d0d9d4be4e4e0f561847c4e41877ce5b6ca
kaeaddaar <playinfc@hotmail.ca>
2017-02-24 01:08:29 -0800

    common commands file created

---
+++ AzureVM/CommonCommands/CommonCommands.ps1
@@ -0,0 +1,6 @@
+﻿Login-AzureRmAccount -SubscriptionName "VSE MPN"
+start-azurermvm -Name "DevApi" -ResourceGroupName "DevApi_Test"
+
+Stop-azurermvm -Name "DevApi" -ResourceGroupName "DevApi_Test"
+
+
